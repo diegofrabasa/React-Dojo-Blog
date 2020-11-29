fcfe16858b0d5e1b1c7f813c16d169299a21aac4
Diego <diegofrabasa@gmail.com>
2020-11-28 20:28:08 -0600

    my-app

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <h1>My first React tag</h1>
+        <p>Welcome :)</p>
+      </div>
+    );
+  }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>My first React tag</h1>
+//       <p>Welcome :)</p>
+//     </div>
+//   );
+// }
+
+export default App;
